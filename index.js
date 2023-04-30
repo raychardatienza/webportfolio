f28bc488f5bb57478678edaca29c950fa6589d27
atienzaraychard <raychardatienza@gmail.com>
2023-04-30 17:56:49 +0800

    change pop-up codes

---
+++ index.js
@@ -0,0 +1,9 @@
+function openPopup(popupId) {
+  var popup = document.getElementById(popupId);
+  popup.style.display = "block";
+}
+
+function closePopup(popupId) {
+  var popup = document.getElementById(popupId);
+  popup.style.display = "none";
+}
