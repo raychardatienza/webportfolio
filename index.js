4af14e7d811ccf5c76450f850cca0f0a21373524
atienzaraychard <raychardatienza@gmail.com>
2023-05-06 21:35:33 +0800

    add message card sent

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
+const contactForm = document.querySelector('#contact-form');
+const contactCard = document.querySelector('#contact-card');
+const messageSentCard = document.querySelector('#message-sent-card');
+const messageSentName = document.querySelector('#message-sent-name');
+const messageSentContact = document.querySelector('#message-sent-contact');
+const messageSentEmail = document.querySelector('#message-sent-email');
+const messageSentMessage = document.querySelector('#message-sent-message');
+
+contactForm.addEventListener('submit', (event) => {
+  event.preventDefault(); // Prevent the default form submission
+  
+  // Get the form values and update the 'Message Sent' card
+  const name = document.querySelector('#name').value;
+  const email = document.querySelector('#email').value;
+  const message = document.querySelector('#message').value;
+  
+  // Update the 'Message Sent' card with the form values
+  messageSentName.textContent = name;
+  messageSentEmail.textContent = email;
+  messageSentMessage.textContent = message;
+  
+  // Show the 'Message Sent' card and hide the contact card
+  contactCard.classList.add('d-none');
+  messageSentCard.classList.remove('d-none');
+  
+  // Alternatively, you can submit the form to the server using AJAX
+  });
+
+
+
+
+
 function openPopup(popupId) {
   var popup = document.getElementById(popupId);
   popup.style.display = "block";
